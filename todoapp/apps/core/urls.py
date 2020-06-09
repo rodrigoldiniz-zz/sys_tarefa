5e9bcd308dddb2c5b4bf3f620799430b95189711
Rodrigo Diniz <rodrigoldiniz@gmail.com>
2020-06-08 21:18:59 -0300

    Implementando busca de tarefa

--- todoapp/apps/core/urls.py
+++ todoapp/apps/core/urls.py
@@ -8,5 +8,6 @@ from . import views
 
 
 urlpatterns = [
+    path('buscar/', views.search_tasks, name='search_tasks'),
     path('', views.home, name='home'),
 ]
--- todoapp/apps/core/views.py
+++ todoapp/apps/core/views.py
@@ -11,3 +11,14 @@ def home(request):
     tasks = Task.objects.filter(owner=request.user).filter(end_date=datetime.today())
     context['tasks'] = tasks
     return render(request, template_name, context)
+
+
+@login_required(login_url='/contas/login/')
+def search_tasks(request):
+    template_name = 'core/search_tasks.html'
+    query = request.GET.get('query')
+    tasks = Task.objects.filter(name__icontains=query, owner=request.user)
+    context = {
+        'tasks': tasks
+    }
+    return render(request, template_name, context)
